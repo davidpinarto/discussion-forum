7ed12478c749ca085c7e36e9c1b6b2eaa4b9e802
David Pinarto <davidpinarto90@gmail.com>
2024-04-22 13:42:49 +0800

    fix: wrong footer component section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { MainHeader } from './MainHeader';
 import { MainBody } from './MainBody';
-import { Footer } from './Footer';
 
 export function Main() {
   return (
     <main>
       <MainHeader />
       <MainBody />
-      <Footer />
     </main>
   );
 }
