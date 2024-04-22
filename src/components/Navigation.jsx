ee628d6f67d973f731aeac1e4e66941bebc70725
David Pinarto <davidpinarto90@gmail.com>
2024-04-22 14:08:18 +0800

    add new element on header

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaSun } from 'react-icons/fa';
 
 export function Navigation() {
+  const [selectedValue, setSelectedValue] = useState('Indonesia');
+
+  const handleSelectChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
   return (
     <nav>
       <ul>
+        <li>
+          <select value={selectedValue} onChange={handleSelectChange}>
+            <option value="Indonesia">Indonesia</option>
+            <option value="English">English</option>
+          </select>
+        </li>
         <li><FaSun /></li>
-        {/* language icon */}
         <li>
           <img src="https://ui-avatars.com/api/?name=David+Pi" alt="avatar" />
+          <div className="profile-info">
+            <h3>Guest</h3>
+            <p>Login</p>
+          </div>
         </li>
       </ul>
     </nav>
