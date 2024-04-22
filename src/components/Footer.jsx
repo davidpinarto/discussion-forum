9c5d4368d9f25cc78d450ffeac3fa15538fd9666
David Pinarto <davidpinarto90@gmail.com>
2024-04-22 13:15:18 +0800

    add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Footer() {
+  return (
+    <footer><strong>created by David Pinarto</strong></footer>
+  );
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { MainHeader } from './MainHeader';
 import { MainBody } from './MainBody';
+import { Footer } from './Footer';
 
 export function Main() {
   return (
     <main>
       <MainHeader />
       <MainBody />
+      <Footer />
     </main>
   );
 }
