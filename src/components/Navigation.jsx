d9c7d7e4483f128a32403e04d47ddf644cb0583a
David Pinarto <davidpinarto90@gmail.com>
2024-04-23 16:20:55 +0800

    delete unused code

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FaSun, FaMoon } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import { setThemeLightActionCreator, setThemeDarkActionCreator } from '../states/theme/action';
