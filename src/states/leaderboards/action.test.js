9e149fc98e4325415af1e37fae544509582d13ea
David Pinarto <davidpinarto90@gmail.com>
2024-04-29 18:16:28 +0800

    integration testing on leaderboards implemented

---
+++ src/states/leaderboards/action.test.js
@@ -0,0 +1,64 @@
+import {
+  describe, beforeEach, afterEach, it, vi, expect,
+} from 'vitest';
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+import api from '../../utils/api';
+import { asyncGetLeaderboards, setLeaderboardsActionCreator } from './action';
+
+const fakeErrorResponse = new Error('Ups, something went wrong');
+
+describe('leaderboards thunk', () => {
+  beforeEach(() => {
+    api._getLeaderboards = api.getLeaderboards;
+  });
+
+  afterEach(() => {
+    api.getLeaderboards = api._getLeaderboards;
+
+    delete api._getLeaderboards;
+  });
+
+  it('should dispatch action and call alert correctly when data fetching is failed', async () => {
+    api.getLeaderboards = () => Promise.reject(fakeErrorResponse);
+    const dispatch = vi.fn();
+    window.alert = vi.fn();
+
+    await asyncGetLeaderboards()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+    expect(window.alert).toHaveBeenCalledWith(fakeErrorResponse.message);
+  });
+
+  it('should dispatch action correctly when data fetching is success', async () => {
+    const fakeGetLeaderboardsResponse = [
+      {
+        user: {
+          id: 'users-1',
+          name: 'John Doe',
+          email: 'john@example.com',
+          avatar: 'https://generated-image-url.jpg',
+        },
+        score: 10,
+      },
+      {
+        user: {
+          id: 'users-2',
+          name: 'Jane Doe',
+          email: 'jane@example.com',
+          avatar: 'https://generated-image-url.jpg',
+        },
+        score: 5,
+      },
+    ];
+    api.getLeaderboards = () => Promise.resolve(fakeGetLeaderboardsResponse);
+    const dispatch = vi.fn();
+
+    await asyncGetLeaderboards()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch)
+      .toHaveBeenCalledWith(setLeaderboardsActionCreator(fakeGetLeaderboardsResponse));
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+  });
+});
