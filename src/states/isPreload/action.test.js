c6086ea7b9bb557bdf921411ec4a14842ad86660
David Pinarto <davidpinarto90@gmail.com>
2024-04-29 18:08:10 +0800

    integration test on isPreload implemented

---
+++ src/states/isPreload/action.test.js
@@ -0,0 +1,51 @@
+import {
+  describe, beforeEach, afterEach, it, vi, expect,
+} from 'vitest';
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+import api from '../../utils/api';
+import { setAuthUserActionCreator } from '../authUser/action';
+import { asyncPreloadProcess, setIsPreloadActionCreator } from './action';
+
+const fakeErrorResponse = new Error('Ups, something went wrong');
+
+describe('isPreload thunk', () => {
+  beforeEach(() => {
+    api._getOwnProfile = api.getOwnProfile;
+  });
+
+  afterEach(() => {
+    api.getOwnProfile = api._getOwnProfile;
+
+    delete api._getOwnProfile;
+  });
+
+  it('should dispatch action and set authUser to null when data fetching is failed then set isPreload to be false', async () => {
+    api.getOwnProfile = () => Promise.reject(fakeErrorResponse);
+    const dispatch = vi.fn();
+
+    await asyncPreloadProcess()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith(setAuthUserActionCreator(null));
+    expect(dispatch).toHaveBeenCalledWith(setIsPreloadActionCreator(false));
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+  });
+
+  it('should dispatch action correctly when data fetching is success', async () => {
+    const fakeGetOwnProfileResponse = {
+      id: 'users-1',
+      name: 'John Doe',
+      email: 'john@example.com',
+      avatar: 'https://generated-image-url.jpg',
+    };
+    api.getOwnProfile = () => Promise.resolve(fakeGetOwnProfileResponse);
+    const dispatch = vi.fn();
+
+    await asyncPreloadProcess()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith(setAuthUserActionCreator(fakeGetOwnProfileResponse));
+    expect(dispatch).toHaveBeenCalledWith(setIsPreloadActionCreator(false));
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+  });
+});
